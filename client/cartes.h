bf7709e650f6ea787480a9e6898f32d13d7542ab
Bacmel <hugo.duarte91800@gmail.com>
2020-01-26 14:46:58 +0100

    modif client

---
+++ client/cartes.h
@@ -0,0 +1,19 @@
+#ifndef CARTES_H
+#define CARTES_H
+
+extern char *nbobjets[8] = {"5", "5", "5", "5", "4", "3", "3", "3"};
+extern char *nbnoms[13] = {"Sebastian Moran",
+                  "irene Adler",
+                  "inspector Lestrade",
+                  "inspector Gregson",
+                  "inspector Baynes",
+                  "inspector Bradstreet",
+                  "inspector Hopkins",
+                  "Sherlock Holmes",
+                  "John Watson",
+                  "Mycroft Holmes",
+                  "Mrs. Hudson",
+                  "Mary Morstan",
+                  "James Moriarty"};
+
+#endif // !CARTES_H
